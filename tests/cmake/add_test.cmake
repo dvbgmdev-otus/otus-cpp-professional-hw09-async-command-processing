c848640c212554eab8e4c170f7bfc82c1c33d436
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2026-07-02 12:34:39 +0300

    refactor(tests): target настройки переведены на helper функции

--- tests/cmake/add_test.cmake
+++ tests/cmake/add_test.cmake
@@ -12,13 +12,8 @@ function(add_unit_test TEST_NAME)
         RUNTIME_OUTPUT_DIRECTORY ${TEST_BIN_DIR}
     )
 
-    target_compile_options(${TEST_NAME} PRIVATE ${WARN_FLAGS})
-
-    # Включить Coverage для тестов
-    if (BUILD_COVERAGE)
-        target_compile_options(${TEST_NAME} PRIVATE ${COVERAGE_COMPILE_FLAGS})
-        target_link_options(${TEST_NAME} PRIVATE ${COVERAGE_LINK_FLAGS})
-    endif()
+    configure_compile_target(${TEST_NAME})
+    configure_link_target(${TEST_NAME})
 
     # add_test(NAME ${TEST_NAME} COMMAND ${TEST_NAME})
     gtest_discover_tests(${TEST_NAME})
