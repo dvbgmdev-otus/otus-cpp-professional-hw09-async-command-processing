b60f4fee75db08706906272757155f0e1dd4eba9
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2026-07-09 06:56:50 +0300

    cleanup(cli): убрана лишняя передача std::cin

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,5 @@
 #include <cstddef>
 #include <iostream>
-#include <istream>
 
 #include "async.h"
 #include "cli_args.h"
@@ -10,14 +9,14 @@ namespace {
 constexpr int EXIT_SUCCESS_CODE = 0;
 constexpr int EXIT_ERROR_CODE = 1;
 
-void process_input(std::istream& input, std::size_t block_size) {
+void process_input(std::size_t block_size) {
     constexpr std::size_t BUFFER_SIZE = 4096;
     char buffer[BUFFER_SIZE];
 
     async::handle_t handle = async::connect(block_size);
-    while (input) {
-        input.read(buffer, BUFFER_SIZE);
-        const std::streamsize received = input.gcount();
+    while (std::cin) {
+        std::cin.read(buffer, BUFFER_SIZE);
+        const std::streamsize received = std::cin.gcount();
         if (received > 0) {
             async::receive(handle, buffer, static_cast<std::size_t>(received));
         }
@@ -34,7 +33,7 @@ int main(int argc, const char* const argv[]) {
         return EXIT_ERROR_CODE;
     }
 
-    process_input(std::cin, cli_args.block_size);
+    process_input(cli_args.block_size);
 
     return EXIT_SUCCESS_CODE;
 }
